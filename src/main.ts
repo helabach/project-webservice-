f647320e2e152d6c71d33b346fb2e8622bbd4715
helabach <98784198+helabach@users.noreply.github.com>
2025-06-08 14:20:02 +0000

    Create project, added needed dependecies , add database configuration using docker compose

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+  const config = new DocumentBuilder()
+    .setTitle('Web Service project')
+    .setDescription('A project for managing job offers and candidate applications using **NestJS** and **GraphQL**.')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+  await app.listen(parseInt(process.env.PORT!));
+}
+bootstrap();
